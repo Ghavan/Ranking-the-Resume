7ea0e806f5bd347346ded8db338af2b3f0eb07d5
Ghavan <ghavanpatel@gecg28.ac.in>
2021-04-11 20:12:09 +0530

    Updating NLP.py file

--- Flask/recruiterAid/NLP.py
+++ Flask/recruiterAid/NLP.py
@@ -1,8 +1,7 @@
 from pyresparser import ResumeParser
-data = ResumeParser('sampo.pdf').get_extracted_data()
-# data = ResumeParser('C:/Users/Dell/Desktop/Projects/FinalYearProject/Git/Flask/recruiterAid/static/ResumeFiles/
-#                     sampd.pdf', skills_file='C:/Users/Dell/Desktop/Projects/FinalYearProject/Git/Flask/recruiterAid/'
-#                                              'static/skills.csv').get_extracted_data()
+# data = ResumeParser('sampo.pdf').get_extracted_data()
+data = ResumeParser('C:/Users/Dell/Desktop/Projects/FinalYearProject/Git/Flask/recruiterAid/static/ResumeFiles/sampo.pdf',
+                    skills_file='C:/Users/Dell/Desktop/Projects/FinalYearProject/Git/Flask/recruiterAid/static/skills.csv').get_extracted_data()
 
 print('\n')
 print('Name: ', data['name'])
