c4d7b317714b356c73dc520d0ec2ce23ba1bd96a
Ghavan <ghavanpatel@gecg28.ac.in>
2021-04-11 19:31:55 +0530

    NLP.py file to update

---
+++ NLP/pyresparser/__init__.py
@@ -0,0 +1,9 @@
+from pyresparser import utils
+from pyresparser import constants
+from pyresparser.resume_parser import ResumeParser
+
+__all__ = [
+    'utils',
+    'constants',
+    'ResumeParser'
+]
